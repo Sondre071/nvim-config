531bca19adf98657b195373925616209ed769292
Sondre071 <sirciller@gmail.com>
2026-07-27 21:06:35 +0200

    resolve keybind conflicts with wezterm

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,19 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local function set_user_var(name, value)
+    io.write(string.format("\027]1337;SetUserVar=%s=%s\007", name, vim.base64.encode(value)))
+end
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+    callback = function()
+        set_user_var("IS_NVIM", "true")
+    end,
+})
+
+vim.api.nvim_create_autocmd("VimLeave", {
+    callback = function()
+        set_user_var("IS_NVIM", "false")
+    end,
+})
