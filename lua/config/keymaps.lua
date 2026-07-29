d8157555b3f53405c0fbd584cd6f08d9abe2fab3
Sondre071 <sirciller@gmail.com>
2026-07-29 21:19:50 +0200

    add keybindings for os clipboard yank, paste

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("v", "<leader>y", '"+y', { desc = "Yank selection to clipboard" })
+vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste from clipboard" })
+vim.keymap.set("v", "<leader>p", '"+p', { desc = "Paste from clipboard, replacing selection" })
